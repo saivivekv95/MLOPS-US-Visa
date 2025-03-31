7be224a0f5b1873370549004468324c9cbb0be10
Vivek <saivivekv95@gmail.com>
2025-03-31 19:19:30 +0200

    Requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Vivek",
+    author_email="saivivekv95@gmail.com.com",
+    packages=find_packages()
+)
